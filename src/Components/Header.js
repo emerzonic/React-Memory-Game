d6bc05c8024c8ac40fd81735664a433dcb8859a1
Emerson Doyah <emerson.doyah@gmail.com>
2018-07-28 22:57:33 -0500

    refactor

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,14 +3,14 @@ import '../App.css';
 
 const Header = () => {
     return (
-<div className="ui grid">
-  <div className="sixteen wide centered column App-header">
-      <div className="headerContent">
-          <h1 className="App-title">Memory Game!</h1>
-          <h3 className="App-intro">Click each crystal to earn points, but don't click on any more than once!</h3>
-      </div>
-  </div>
-</div>
+        <div className="ui grid">
+            <div className="sixteen wide centered column App-header">
+                <div className="headerContent">
+                    <h1 className="App-title">Memory Game!</h1>
+                    <h3 className="App-intro">Click each crystal to earn points, but don't click on any more than once!</h3>
+                </div>
+            </div>
+          </div>
     );
   }
 
