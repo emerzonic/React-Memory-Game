e8680366c436f6c5ecc3b6c195eb62e0ed544785
Emerson Doyah <emerson.doyah@gmail.com>
2018-07-30 07:08:52 -0500

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import '../App.css';
 
-const Header = () => {
-    return (
+const Header = () => 
+     (
         <div className="ui raised segments">
              <div className="ui segment centered">
                 <div className="App-header">
                     <h1 className="App-title">Memory Game!</h1>
-                    <h3 className="App-intro">Click each crystal to earn points, but don't click on any more than once!</h3>
+                    <h3 className="App-intro"> Click each crystal to earn points, but do not click on any more than once!</h3>
                 </div>
             </div>
         </div>
     );
-  }
+  
 
 
 export default Header; 
