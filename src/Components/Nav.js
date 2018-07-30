1ae12a83911d0d5b0ce98c278ea9ad7b05db066e
Emerson Doyah <emerson.doyah@gmail.com>
2018-07-30 07:10:39 -0500

    Update Nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../App.css';
 
-const Nav = (props) => {
-  return (
+const Nav = props => 
+        (
         <div className="ui three item menu fixed top">
             <li className="item App-nav">Memory Game</li>
             <li className="item App-nav"> {props.nav.message}</li>
             <li className="item App-nav">Score: {props.nav.score} | Top Score: {props.nav.topScore}</li>
         </div>
-    );
-  }
+       );
+  
 
   export default Nav;
